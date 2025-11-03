67174bbd96efa3244135058eaac0e6e82f97ad09
emiliomaset <145236437+emiliomaset@users.noreply.github.com>
2025-11-02 21:55:03 -0500

    committing ugh. can't get WWW options to work

--- src/components/WWW.tsx
+++ src/components/WWW.tsx
@@ -1,11 +1,32 @@
 import Select from "react-select/base";
-
+import {useEffect, useState} from "react";
+import {API_BASE_URL} from "../config.ts";
 
 function WWW() {
+
+    const [rawContent, setRawContent] = useState([])
+    const [contentArray, setContentArray] = useState([])
+    const [firstRender, setFirstRender] = useState(true)
+
+    useEffect( () => {
+       fetch(`${API_BASE_URL}/getallcontent`).then(response => response.json()).
+       then(data => {
+           setFirstRender(false)
+           setRawContent(data)
+           rawContent.forEach( (content, index) => setContentArray(...contentArray, {value: content.title, label: content.contentId}));
+       })
+        }, [firstRender]);
+
+    console.log("raw", rawContent)
+    console.log("?", contentArray)
+
+
+
     return(
         <>
 
-                <Select/>
+
+                <Select options={contentArray}/>
 
 
         </>
