f8a9600bef3084484640c7fd7014a41059c8c695
emiliomaset <145236437+emiliomaset@users.noreply.github.com>
2025-11-04 17:40:20 -0500

    pushing before dinner. having trouble getting text from getmovieorseries api call

--- src/components/WWW.tsx
+++ src/components/WWW.tsx
@@ -2,25 +2,44 @@ import Select from "react-select";
 import {useEffect, useState} from "react";
 import {API_BASE_URL} from "../config.ts";
 
-
-
 function WWW() {
     const [options, setOptions] = useState([]);
+    const [rawContent, setRawContent] = useState([])
+    const [idOfContentSelected, setIdOfContentSelected] = useState("")
+    const [typeOfContentSelected, setTypeOfContentSelected] = useState("")
     
-    useEffect(() => {
+    useEffect(() => { // load content options
         fetch(`${API_BASE_URL}/getallcontent`)
             .then(response => response.json())
             .then(data => {
-                const formattedOptions = data.map(content => (
+                setRawContent(data)
+                const formattedOptions = data.map((content: { contentId: string; title: string; }) => (
                     {value: content.contentId, label: content.title}
                 ));
                 setOptions(formattedOptions);
             })
-    }, []); 
+    }, []);
+
+    useEffect(() => {
+        fetch(`${API_BASE_URL}/getmovieorseries`, {
+            method: 'POST',
+            headers: {"Content-Type": "text/plain"},
+            body: idOfContentSelected
+        }).then(response => response.text())
+            .then(contentType => setTypeOfContentSelected(contentType))
+
+    console.log(typeOfContentSelected)
+
+    }, [idOfContentSelected]);
+
+
 
     return (
         <>
-            <Select options={options}/>
+            <Select
+                options={options}
+                onChange={(data:string) => setIdOfContentSelected(data.value)}
+            />
         </>
     );
 
