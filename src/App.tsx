3a825d869ab3fbedc69e0ff7d457d43f5e9bf536
bobbybom11 <bobbybom11@yahoo.com>
2025-12-01 11:08:59 -0500

    Resolved merge conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,28 @@ import MemberHome from "./pages/MemberHome.tsx"
 import LandingPage from "./pages/LandingPage.tsx"
 import AppLayout from "./AppLayout.tsx"
 import AdminHome from "./pages/AdminHome.tsx";
-<<<<<<< HEAD
-import AdminAnalytics from "./pages/AdminAnalytics.tsx";
-=======
+
 import WWW from "./components/WWW.tsx";
 import AdminAnalytics from "./components/AdminAnalytics.tsx";
 import BrowsePage from './pages/BrowsePage.tsx';
 
-
->>>>>>> 271911456cc23052c2736941c2656769f18b745d
 function App() {
     return (
         <>
-
-        <BrowserRouter>
-            <AppLayout>
-            <Routes>
-                <Route path="/" element={<LandingPage />}></Route>
-                <Route path="/member/home" element={<MemberHome />} />
-                <Route path="/admin/home" element={<AdminHome />} />
-                <Route path="/www" element={<WWW />} />
-                <Route path="/analytics" element={<AdminAnalytics/>} />
-                <Route path="/browse" element={<BrowsePage/>} />
-            </Routes>
-            </AppLayout>
-        </BrowserRouter>
-
-
+            <BrowserRouter>
+                <AppLayout>
+                    <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                        <Route path="/member/home" element={<MemberHome />} />
+                        <Route path="/admin/home" element={<AdminHome />} />
+                        <Route path="/www" element={<WWW />} />
+                        <Route path="/analytics" element={<AdminAnalytics />} />
+                        <Route path="/browse" element={<BrowsePage />} />
+                    </Routes>
+                </AppLayout>
+            </BrowserRouter>
         </>
-    ) // end of return
-} // end of App
+    );
+}
 
-export default App
+export default App;
